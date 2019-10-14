2578e97d4c17b5501398cfeeecc190bdbf13d3e4
Mohammad Faisal <shahfaisalsmart1234@gmail.com>
2019-10-15 00:13:15 +0530

    new file added

---
+++ PrimeSubtraction.cpp
@@ -0,0 +1,20 @@
+// Author:: Mohammmad Faisal
+// BEGINNING WITH THE NAME OF GOD ALLAH
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        long long a,b;
+        cin >> a >> b;
+        if(a-b > 1)
+            cout<<"YES";
+        else
+            cout<<"NO";
+        cout<<"\n";
+    }
+    return 0;
+}
