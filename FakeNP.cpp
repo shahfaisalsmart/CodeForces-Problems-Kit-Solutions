f9cd54cccb4c79ecfc242445dda63bc7fa58ced6
Mohammad Faisal <shahfaisalsmart1234@gmail.com>
2019-10-24 02:32:10 +0530

    new file added

---
+++ FakeNP.cpp
@@ -0,0 +1,18 @@
+
+// AUTHOR ---> MOHAMMAD FAISAL
+// BEGINNING WITH THE NAME OF ALMIGHTY GOD ALLAH
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	int l,r;
+	cin >> l >> r;
+	if(l!=r)
+	cout<<"2"<<endl;
+	else
+	cout<<l<<endl;
+	return 0;
+}
