9e03106b180771d069bf2444452111a61cebc659
Mohammad Faisal <shahfaisalsmart1234@gmail.com>
2019-11-02 13:50:13 +0530

    new file added

---
+++ DominoPilling.cpp
@@ -0,0 +1,18 @@
+// BEGINNING WITH THE NAME OF ALMIGHTY GOD ALLAH
+// AUTHOR:: MOHAMMAD FAISAL
+#include<bits/stdc++.h>
+#include<ctype.h>
+using namespace std;
+ 
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int n, m;
+	cin >> n >> m;
+	int res = (n*m)>>1;
+	cout<<res;
+	return 0;
+}
